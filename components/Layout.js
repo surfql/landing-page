de979cdd4aa24fe2b7355c160c60e2b34d727cbe
Dwayne Neckles <dwayneaneckles@gmail.com>
2022-09-27 21:06:40 -0400

    a few files and graphics

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,18 @@
 import React from "react";
 import Nav from "./Nav";
 import FirstSection from "./FirstSection";
+import Features from "./Features";
+import ProblemsItSolves from "./ProblemsItSolves";
+import Teams from "./Teams";
 
 export default function Layout({ children }) {
   return (
     <div>
       <Nav />
       <FirstSection />
+      <Features />
+      <ProblemsItSolves />
+      <Teams />
       {children}
     </div>
   );
