826111eba55252ad4fb74e11035c29e580a3d7e6
Your Name <tonfroy90@gmail.com>
2022-11-03 15:48:49 -0700

    Fixing Mobile Responsiveness pt1

--- components/Nav.js
+++ components/Nav.js
@@ -4,14 +4,14 @@ import Link from "next/link";
 
 export default function Nav() {
   return (
-    <div className="absolute w-full py-4 px-8 pl-20 font-abc text-white flex justify-between">
-      <div className="sticky">
+    <div className="absolute w-full py-4 pl-20 font-abc text-white flex justify-between">
+      <div>
         <Image src={"/surfql.png"} alt="Logo" width="65" height="65" />
         <span className="text-3xl ml-3">SurfQL</span>
       </div>
 
       <div className="flex items-center pr-10">
-        <div className="btn ml-4 text-lg py-1 px-1 undrlne">
+        <div className="btn text-lg py-1 px-1 undrlne">
           <Link href="">
             <a target="_blank" rel="noreferrer">
               LinkedIn
@@ -33,16 +33,15 @@ export default function Nav() {
           </Link>
         </div>
       </div>
-      <div className="fixed  bottom-0 right-0 p-4 z-10">
-      <footer className=" p-4 w-auto rounded-lg text-2xl bg-[#5fefd0] text-black hover:text-gray-700 hover:bg-gray-50 dark:hover:text-white ">
-        <Link href="https://marketplace.visualstudio.com/items?itemName=SurfQL.surfql">
-          <a target="_blank" rel="noreferrer">
-          Download the extension
-          </a>
-        </Link>
-       
-      </footer>
-      </div>  
+      <div className="fixed  bottom-0 right-1 p-4 z-10">
+        <footer className=" p-4 w-auto rounded-lg text-2xl bg-[#5fefd0] text-black hover:text-gray-700 hover:bg-gray-50 dark:hover:text-white ">
+          <Link href="https://marketplace.visualstudio.com/items?itemName=SurfQL.surfql">
+            <a target="_blank" rel="noreferrer">
+              Download the extension
+            </a>
+          </Link>
+        </footer>
+      </div>
     </div>
   );
 }
